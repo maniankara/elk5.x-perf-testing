0b68ef6239eb9ae78ee9bd50421eaad93b7d552f
anoop <anoop@tuxrea.com>
2017-04-18 23:41:26 +0300

    added empty json template and generate logs and testdata shell scripts

---
+++ tools/generate_logs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+function main {
+    echo "Generating logs every 5 seconds";
+}
+
+main $*;
---
+++ tools/generate_testdata.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+function main {
+    echo "Generating test data for last 30 days";
+
+}
+
+main $*;
