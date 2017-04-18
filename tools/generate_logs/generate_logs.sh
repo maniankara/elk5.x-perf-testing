94142b082ce84ffb89c631a48e4c8df8444747d0
anoop <anoop@tuxrea.com>
2017-04-19 00:09:05 +0300

    Added docker and curl loop commands

--- tools/generate_logs/generate_logs.sh
+++ tools/generate_logs/generate_logs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 function main {
-    echo "Generating logs every 5 seconds";
+    echo "Generating logs every 2 seconds";
+    docker run -d --name apache_server -p 8080:80 httpd:2.4
+    while true; do sleep 2; curl locahost:8080; done
 }
 
 main $*;
